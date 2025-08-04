4e6f647b25bb5d144cddb702fc71561edb262ba0
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-05 01:27:49 +0200

    draft

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+    
+}
