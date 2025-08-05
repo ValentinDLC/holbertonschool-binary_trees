c6dbde7f590a39170c54bdd560a8ce0eb950e9ec
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-05 20:27:14 +0200

    function that inserts a node as the left-child of another node and add comments

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,29 +8,31 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    
-    binary_tree_t *new_node;
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
+binary_tree_t *new_node, *old_left;
+if (parent == NULL)
+{
+return (NULL);
+}
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+/*Initialize the new node*/
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+/*save the current left child*/
+old_left = parent->left;
 /*If parent already has a left-child, the new node must take its place*/
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-/*and the old left-child must be set as the left-child of the new node*/
-        
-    }
-    return (new_node);
-    
+if (old_left != NULL)
+{
+new_node->left = old_left;
+/*the old left-child must be set as the left-child of the new node*/
+old_left->parent = new_node;
+}
+/*update the parent left child to pointer to the new_node*/
+parent->left = new_node;
+return (new_node);
 }
