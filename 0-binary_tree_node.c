9a014a5fb4972e6dd6d5054dee3eb2a9fff8e046
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-06 10:23:33 +0200

    add comment for new node initialization

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
 new_node = malloc(sizeof(binary_tree_t));
+/*checking if we have enough memory to allocate*/
 if (new_node == NULL)
 {
 return (NULL);
 }
+/*Initialize the new node*/
 new_node->n = value;
 new_node->parent = parent;
 new_node->left = NULL;
