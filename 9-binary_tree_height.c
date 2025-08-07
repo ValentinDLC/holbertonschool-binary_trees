ed5e0ebbd8f92f80fc58eee3c7015d8827b73fa9
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-07 16:28:09 +0200

    fix betty coding style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
- * 
+ *
  * Return: Height of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -17,17 +17,17 @@ if (tree->left == NULL && tree->right == NULL)
 {
 return (0);
 }
-/*recursively calculate the height of the left subtree and the height of the right subtree*/
+/*recursively calculate the height of the left and right subtree*/
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 /*Compare the heights of the left and right subtrees*/
 if (left_height > right_height)
 {
-/*If left subtree is taller, +1 for the edge to connect the current node to left child*/
+/* If left subtree is taller, +1 for the edge to the left child */
 return (left_height + 1);
 }
-else 
-/*If right subtree is taller or equal, +1 for the edge to connect the current node to right child*/
+else
+/* If right subtree is taller or equal, +1 for the edge to the right child */
 {
 return (right_height + 1);
 }
