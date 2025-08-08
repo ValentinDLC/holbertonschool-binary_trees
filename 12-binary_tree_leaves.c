a838959a9030214a7900d06299a8db7b22520edd
Rebecca F <rebecca.fsq011@protonmail.com>
2025-08-08 15:26:40 +0200

    fix betty coding style

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - Count the leaves in a tree
- * @tree: Pointer to the root node of the tree to count the number of leaves
- * 
- * Return: Number of leaves in the tree or 0 if tree is NULL
- */
+* binary_tree_leaves - Count the leaves in a tree
+* @tree: Pointer to the root node of the tree to count the number of leaves
+*
+* Return: Number of leaves in the tree or 0 if tree is NULL
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    /* return 1 if left and right subtree is a leaf*/
+if (tree == NULL)
+{
+return (0);
+}
+/* return 1 if left and right subtree is a leaf*/
 if (tree->left == NULL && tree->right == NULL)
 {
-    return (1);
+return (1);
 }
-    /* count recursively the number of leaves in left and right subtree */
+/* count recursively the number of leaves in left and right subtree */
 return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
