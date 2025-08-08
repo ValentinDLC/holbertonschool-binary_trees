1c76c3c405617d8ffb773ca893046451edfadc4e
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-08 20:33:51 +0200

    fix betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,21 +9,22 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return 0;
-    }
-    if (!(tree->left || tree->right))
-    {
-        return 1;
-    }
-    if (!(tree->left == NULL || tree->right == NULL))
-    {
-        /* If the node has both left and right children,
-     * check recursively if both subtrees are full
-     */
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
-/* If the node has only one child, it's not a full tree */
-    return (0);
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (tree->left && tree->right)
+	{
+		/*
+		 * If the node has both left and right children,
+		 * check recursively if both subtrees are full
+		 */
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	}
+
+	/* If the node has only one child, it's not a full tree */
+	return (0);
 }
