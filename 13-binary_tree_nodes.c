237959b098e54506573588700543bc171c539bba
ValentinDLC <valentin.dardenne9@gmail.com>
2025-08-08 21:00:54 +0200

    fix betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,25 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree.
- * @tree: Pointer to the root node of the tree to count the nodes in.
+ * binary_tree_nodes - Counts the nodes with at least one child in a tree
+ * @tree: Pointer to the root node of the tree to count the nodes in
  *
- * Return: Number of nodes with at least one child, or 0 if tree is NULL.
-*/
+ * Return: Number of nodes with at least one child, or 0 if tree is NULL
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count_nodes;
+	size_t count_nodes;
 
-    if (!tree)
-    {
-        return (0);
-    }
-	if (!(tree->left || tree->right))
-	{
+	if (!tree)
 		return (0);
-	}
-/* recursively count nodes subtrees */
-    count_nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) ;
-/* add 1 for the current node, this function count non-leaf nodes */
-    return (1 + count_nodes);
+
+	if (!tree->left && !tree->right)
+		return (0);
+
+	/* Recursively count nodes subtrees */
+	count_nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+
+	/* Add 1 for the current node, this function count non-leaf nodes */
+	return (1 + count_nodes);
 }
